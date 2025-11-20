c9027fd9439cec2da7a602864d48217e57aa1402
glathrom <grant.lathrom@gmail.com>
2025-11-20 07:02:15 -0600

    checking controller code without transmitting

--- controller_code/controller_code.ino
+++ controller_code/controller_code.ino
@@ -4,19 +4,19 @@
 
 // six byte mac address for the controller board obtained
 // from the get_mac_address program
-uint8_t broadcastAddress[] = {0x94, 0xa9, 0x90, 0x7c, 0x40, 0x40};
+uint8_t broadcastAddress[] = {0x94, 0xa9, 0x90, 0x77, 0x6a, 0x64};
 
-float PositionX;
-float PositionY;
-float PositionZ;
+// float PositionX;
+// float PositionY;
+// float PositionZ;
 
 float incomingPosX;
 float incomingPosY;
 float incomingPosZ;
 
+int ch0;
 int ch1;
 int ch2;
-int ch3;
 
 String success;
 
@@ -55,11 +55,14 @@ void setup() {
   Serial.begin(115200);
 
   pinMode(D2, INPUT_PULLUP);
+  Serial.printf("Pin D2 set high\n");
+
   WiFi.mode(WIFI_STA);
   if( esp_now_init() != ESP_OK){
     Serial.printf("Error Initializing ESP-NOW\n");
     return;
   }
+  Serial.printf("esp_now is OK!\n");
 
   esp_now_register_send_cb((esp_now_send_cb_t) OnDataSent);
 
@@ -71,6 +74,7 @@ void setup() {
     Serial.printf("Failed to add peer\n");
     return ;
   }
+  Serial.printf("peer added\n");
 
   esp_now_register_recv_cb((esp_now_recv_cb_t) OnDataReceived);
 }
@@ -78,28 +82,31 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+
+
+  ch0 = analogRead(D0);
+  PosReadings.PosX = map(ch0, 0, 4096, 0, 180);
   ch1 = analogRead(D1);
-  PositionX = map(ch1, 0, 4096, 0, 180);
+  PosReadings.PosY = map(ch1, 0, 4096, 0, 180);
   ch2 = analogRead(D2);
-  PositionY = map(ch1, 0, 4096, 0, 180);
-  ch2 = analogRead(D3);
-  PositionZ = map(ch1, 0, 4096, 0, 180);
+  PosReadings.PosZ = map(ch2, 0, 4096, 0, 180);
 
-  PosReadings.PosX = PositionX;
-  PosReadings.PosY = PositionY;
-  PosReadings.PosZ = PositionZ;
+  // PosReadings.PosX = PositionX;
+  // PosReadings.PosY = PositionY;
+  // PosReadings.PosZ = PositionZ;
 
+  Serial.printf("********************\n");
   Serial.printf("PosX: %f\n", PosReadings.PosX);
   Serial.printf("PosY: %f\n", PosReadings.PosY);
   Serial.printf("PosZ: %f\n", PosReadings.PosZ);
 
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &PosReadings, sizeof(PosReadings));
+  // esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &PosReadings, sizeof(PosReadings));
 
-  if( result == ESP_OK){
-    Serial.printf("Send with success\n");
-  } else {
-    Serial.printf("Error sending data\n");
-  }
+  // if( result == ESP_OK){
+  //   Serial.printf("Send with success\n");
+  // } else {
+  //   Serial.printf("Error sending data\n");
+  // }
 
   delay(100);
 
